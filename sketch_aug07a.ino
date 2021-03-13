2db4499c4b88a982b60028c85313c8d96d26d088
Peter McCool <petermmccool@gmail.com>
2021-03-13 14:44:53 +1000

    Lower target temp, warm up to a certain temp before using PID controller, refactoring

--- sketch_aug07a.ino
+++ sketch_aug07a.ino
@@ -9,8 +9,8 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-#define D3 32
-#define D4 33
+#define statusLEDPin 32
+#define popperPowerSwitch 33
 
 int thermocoupleSO = 19;
 int thermocoupleCS = 23;
@@ -41,6 +41,7 @@ WebServer server(80);
  *  setpoint,current_time,thermocouple_reading,ambient_temperature,humidity,relay_state.
  *  Temperatures are in Celsius.
  *  It uses a PID controller to decide when to switch the popper on/off
+ *  ESP32 pinout: https://www.instructables.com/ESP32-Internal-Details-and-Pinout/
  *    
  */
 
@@ -67,7 +68,7 @@ std::vector<setpoint_record> setpoint_records_general2 = {
   {900 *1000,220}};
 
 // started life as a profile for the Chesterton, but I find I prefer the results :)
-  std::vector<setpoint_record> setpoint_records_general = {
+  std::vector<setpoint_record> setpoint_records_general3 = {
   {60*1000, 120},
   {120 *1000,120},
   {180 *1000,140},
@@ -83,7 +84,42 @@ std::vector<setpoint_record> setpoint_records_general2 = {
   {780 *1000,210},
   {840 *1000,215}};
 
-std::vector<setpoint_record> setpoint_records_chesterton = {
+std::vector<setpoint_record> setpoint_records_general = {
+  {60*1000, 100},
+  {120 *1000,110},
+  {180 *1000,120},
+  {240 *1000,130},
+  {300 *1000,140},
+  {360 *1000,150},
+  {420 *1000,160},
+  {480 *1000,170},
+  {540 *1000,180},
+  {600 *1000,190},
+  {660 *1000,200},
+  {720 *1000,207},
+  {780 *1000,214},
+  {840 *1000,220}};
+
+
+std::vector<setpoint_record> setpoint_records_slow = {
+  {60*1000, 100},
+  {120 *1000,110},
+  {180 *1000,120},
+  {240 *1000,130},
+  {300 *1000,140},
+  {360 *1000,150},
+  {420 *1000,160},
+  {480 *1000,170},
+  {540 *1000,180},
+  {600 *1000,190},
+  {660 *1000,195},
+  {720 *1000,200},
+  {780 *1000,205},
+  {840 *1000,210},
+  {900 *1000,215},
+  {900 *1000,220}};
+
+std::vector<setpoint_record> setpoint_records_chesterton2 = {
   {60*1000, 120},
   {120 *1000,120},
   {180 *1000,140},
@@ -102,8 +138,8 @@ std::vector<setpoint_record> setpoint_records_chesterton = {
 void setup()
 {
   Serial.begin(9600);
-  pinMode(D4, OUTPUT);
-  pinMode(D3, OUTPUT);
+  pinMode(popperPowerSwitch, OUTPUT);
+  pinMode(statusLEDPin, OUTPUT);
   bool status = ambient_temperature_sensor.begin(0x77); // I think? or 0x76?
   if (!status) {
     Serial.println("Could not find ambient temperature sensor");
@@ -149,18 +185,30 @@ double calculate_setpoint(int time, std::vector<setpoint_record> setpoint_record
 void pid_controller() {
   // based on https://github.com/br3ttb/Arduino-PID-Library/blob/master/examples/PID_RelayOutput/PID_RelayOutput.ino
   double setpoint, input, output;
-  int target_temp = 216;
+  //int target_temp = 208;
+  int target_temp = 203; // latest attempt at making Chesterton not taste of ash :)
+  int start_temp = 120;
 
-  digitalWrite(D3, HIGH);
-  //Specify the links and initial tuning parameters
+  // The point here is to get up to operating temperature ASAP. The thinking here is that the PID controller will behave more
+  // predictably if the temp is roughtly at the first setpoint when it starts. So just run the popper flat-out till it gets there :)
+  // the counter is just a really cheap & nasty way of making sure the popper doesn't stay on indefinitely
+  digitalWrite(statusLEDPin, HIGH);
+  int count = 0;
+  Serial.print("Warming up to " + start_temp + "\n");
+  while(thermocouple.readCelsius() < start_temp && count < 20) {
+    radioSwitch(popperPowerSwitch, 1, true);
+    count++;
+  }
+
+  radioSwitch(popperPowerSwitch, 1, false);
+ 
   double kp=5, ki=3, kd=1;
   PID my_pid(&input, &output, &setpoint, kp, ki, kd, DIRECT);
-
   int window_size = 20000;
   int window_start_time = millis();
 
   //initialize the variables we're linked to
-  setpoint = calculate_setpoint(millis() - window_start_time, setpoint_records_general, target_temp);
+  setpoint = calculate_setpoint(millis() - window_start_time, setpoint_records_slow, target_temp);
 
   //tell the PID to range between 0 and the full window size
   my_pid.SetOutputLimits(0, window_size);
@@ -171,18 +219,18 @@ void pid_controller() {
 
   // bail when we hit target temp, or have been running for 20 minutes.
   while(input <= target_temp && millis() <= 20*60*1000) {
-    setpoint = calculate_setpoint(millis() - window_start_time, setpoint_records_general, target_temp);
+    setpoint = calculate_setpoint(millis() - window_start_time, setpoint_records_slow, target_temp);
     my_pid.Compute();
 
     bool state = output > setpoint;
-    radioSwitch(D4, 1, state);
+    radioSwitch(popperPowerSwitch, 1, state);
     input = logStateAndReturnTemperature(0, setpoint, state);
     server.handleClient();
   }
   Serial.print("Complete\n");
-  digitalWrite(D3, LOW);
+  digitalWrite(statusLEDPin, LOW);
   while(true) {
-    radioSwitch(D4, 1, false); //make sure it finishes in an off state
+    radioSwitch(popperPowerSwitch, 1, false); //make sure it finishes in an off state
     server.handleClient();
   }
 }
@@ -194,11 +242,12 @@ void handleRoot() {
 
 void setup_webserver() {
   delay(1000);
-  Serial.print("Configuring access point...");
-  /* You can remove the password parameter if you want the AP to be open. */
+  Serial.print("Configuring access point on");
+  Serial.print(ssid);
+  Serial.print(password);
   WiFi.softAP(ssid, password);
 
-  IPAddress myIP = WiFi.localIP();
+  IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
   server.on("/", handleRoot);
